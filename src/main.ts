dfa099a3e60135f6dfa709830390b43e7de14cba
Stanley <41586666+repository@users.noreply.github.com>
2020-11-06 18:51:47 -0800

    link parsing

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
-console.log("bruh");
+import dotenv from "dotenv";
+import Eris from "eris";
+
+dotenv.config();
+
+const PIXIV_REGEX = /https?:\/\/(?:www\.|)pixiv\.net\/(?:en\/|)artworks\/(\d+)/g;
+
+const token = process.env["TOKEN"];
+if (!token) throw "Token not specified";
+
+const bot = Eris(token);
+
+bot.on("ready", () => {
+  console.log("connected to gateway");
+});
+
+bot.on("messageCreate", (m) => {
+  if (m.author.bot) return;
+
+  const links = PIXIV_REGEX.exec(m.content);
+  if (!links) return;
+
+  console.log(links);
+});
+
+bot.connect();
